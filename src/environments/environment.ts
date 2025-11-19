49bdb2a268adfff34767b553a6385d9f9a363e0d
DEBMALYAMAJUMDER <96230712+DEBMALYAMAJUMDER@users.noreply.github.com>
2025-11-19 13:35:32 +0530

    Change environment variable from Dev to production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 
 export const environment = {
-  Dev: true,
+  production: false,
   apiBaseUrl: 'https://mathtrix-hackathon.onrender.com/api/v1' // optional base, if empty uses localhost defaults
 };
